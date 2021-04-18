355f8f05987ca486c0416932ac1550150a41e9e5
AlexGvozdik <77895722+AlexGvozdik@users.noreply.github.com>
2021-04-18 17:25:28 +0000

    Deploying to gh-pages from @ AlexGvozdik/project-team@0722b97752b943e7562c3d73f81be405b964d018 🚀

--- src.c8e957c4.js
+++ src.c8e957c4.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-team/src.30a6139d.js.map
+//# sourceMappingURL=/project-team/src.c8e957c4.js.map
